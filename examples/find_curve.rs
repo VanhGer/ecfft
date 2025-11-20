0f730e6e303a77b7cd9d339d3af12fd86b601b62
vanhger <vietanhpg2003@gmail.com>
2025-11-20 09:06:43 +0700

    feat: bn254 curve

--- examples/find_curve.rs
+++ examples/find_curve.rs
@@ -4,33 +4,18 @@ use ark_ff::Field;
 use ecfft::ec::GoodCurve;
 use ecfft::ec::Point;
 use ecfft::find_curve::find_curve;
-use ecfft::secp256k1::Fp;
-use std::sync::atomic::AtomicU32;
-use std::sync::atomic::Ordering;
 
+use ark_bn254::Fr as Fp;              // <--- BN254 scalar field
 fn main() {
-    let max_n = AtomicU32::new(10);
-
-    rayon::scope(|s| {
-        for _ in 0..10 {
-            s.spawn(|_| {
-                let mut rng = rand::thread_rng();
-                loop {
-                    let (n, g) = find_curve::<Fp>(&mut rng, max_n.load(Ordering::Relaxed) + 1);
-                    if n > max_n.load(Ordering::Relaxed) {
-                        max_n.store(n, Ordering::Relaxed);
-                        let curve = g.curve.unwrap();
-                        match curve {
-                            GoodCurve::Odd { a, b } => {
-                                let bb = b.square();
-                                let Point { x, y, .. } = g;
-                                println!("n={n}, a = {a}, bb = {bb}, p=({x}, {y})",)
-                            }
-                            GoodCurve::Even { b: _ } => todo!(),
-                        }
-                    }
-                }
-            });
+        let mut rng = rand::thread_rng();
+        let (n, g) = find_curve::<Fp>(&mut rng, 28);
+        let curve = g.curve.unwrap();
+        match curve {
+            GoodCurve::Odd { a, b } => {
+                let bb = b.square();
+                let Point { x, y, .. } = g;
+                println!("n={n}, a = {a}, bb = {bb}, p=({x}, {y})",)
+            }
+            GoodCurve::Even { b: _ } => todo!(),
         }
-    });
 }
